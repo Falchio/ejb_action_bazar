7be03e751f91f49e15ce58b31a8a7c35ad73f9ab
Falchio <soulsard@gmail.com>
2022-07-25 00:05:09 +0300

    refactoring

--- src/main/java/com/github/falchio/myactionbazaar/Echo.java
+++ src/main/java/com/github/falchio/myactionbazaar/Echo.java
@@ -4,8 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello")
-public class HelloResource {
+@Path("/echo")
+public class Echo {
     @GET
     @Produces("text/plain")
     public String hello() {
